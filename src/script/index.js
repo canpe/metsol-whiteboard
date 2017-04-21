a0804c4a26e53d87c2900fafdaa8d4227947158d
canpe <can.peskersoy@qlogic.se>
2017-04-21 18:33:27 +0200

    Fixed: Overlay of the widgets

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,10 @@
 $(function () {
     var options = {
+        width:9,
+        height:7,
+        animate: true,
         cell_height: 110,
-        vertical_margin: 10
+        vertical_margin: 15,
     };
     $('.grid-stack').gridstack(options);
     $('.k-button').kendoButton();
